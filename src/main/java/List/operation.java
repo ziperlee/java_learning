dcbf13e2159d81a6c1fb671b4fa7718cddc10d59
liwei <wei.li001986@tongdun.net>
2020-03-09 14:33:22 +0800

    Add List 差集，并集，合并

--- src/main/java/List/operation.java
+++ src/main/java/List/operation.java
@@ -22,9 +22,46 @@ public class operation {
         System.out.println(l1);
         System.out.println(l2);
         System.out.println(l3);
+        System.out.println('\n');
     }
+    // 差集
+    public static void subtract() {
+        List l1 = asList("hello", "hehe");
+        List l2 = asList("hello", "hehe2");
+        List l3 = ListUtils.subtract(l1, l2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println('\n');
+    }
+
+    // 并集
+    public static void sum() {
+        List l1 = asList("hello", "hehe");
+        List l2 = asList("hello", "hehe2");
+        List l3 = ListUtils.sum(l1, l2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println('\n');
+    }
+
+    // 合并
+    public static void union() {
+        List l1 = asList("hello", "hehe");
+        List l2 = asList("hello", "hehe2");
+        List l3 = ListUtils.union(l1, l2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println('\n');
+    }
+
 
     public static void main(String[] args) {
         intersection();
+        subtract();
+        sum();
+        union();
     }
 }
