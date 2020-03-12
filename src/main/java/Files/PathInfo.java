29441a801a3577e14ba939617386432305b1eaec
liwei <wei.li001986@tongdun.net>
2020-03-12 17:04:18 +0800

    Add Files

---
+++ src/main/java/Files/PathInfo.java
@@ -0,0 +1,50 @@
+package Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*; //Files
+
+/**
+ * description
+ *
+ * @author liwei
+ * @createTime 2020/03/12
+ */
+
+public class PathInfo {
+    static void show(String id, Object p) {
+        System.out.println(id + ": " + p);
+    }
+
+    static void info(Path p) {
+        show("toString", p);
+        show("Exists", Files.exists(p));
+        show("RegularFile", Files.isRegularFile(p));
+        show("Directory", Files.isDirectory(p));
+        show("Absolute", p.isAbsolute());
+        show("FileName", p.getFileName());
+        show("Parent", p.getParent());
+        show("Root", p.getRoot());
+        System.out.println("******************");
+    }
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
+        info(Paths.get("C:", "path", "to", "nowhere", "NoFile.txt"));
+        Path p = Paths.get("PathInfo.java");
+        info(p);
+        Path ap = p.toAbsolutePath();
+        info(ap);
+        info(ap.getParent());
+        try {
+            info(p.toRealPath());
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+        URI u = p.toUri();
+        System.out.println("URI: " + u);
+        Path puri = Paths.get(u);
+        System.out.println(Files.exists(puri));
+        File f = ap.toFile(); // Don't be fooled
+    }
+}
